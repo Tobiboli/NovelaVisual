ac204f48ddfb97fce65d5c7201580b1f6c569bcb
polaruwu242 <le202446037@alm.buap.mx>
2026-04-22 18:34:08 -0600

    agregue ka funcion de que el boton de finales lleva a otro escenario para sistema de guardado

--- Assets/scripts/menu/menu.cs
+++ Assets/scripts/menu/menu.cs
@@ -5,8 +5,12 @@ public class Menu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void jugar()
     {
-     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }  
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void finales()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+}
     public void salir()
     {
         Debug.Log("Saliendo del juego...");
