e7dd30d91a82a6feb3df44167423a87dfaaaca85
Tobiboli <eb202442495@alm.buap.mx>
2026-04-25 19:34:49 -0600

    Acomode assent de ilustraciones

--- Assets/scripts/escenarios/cafetera.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class cafetera : MonoBehaviour
-{
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        GetComponent<Renderer>().material.color = Color.cyan;
-    }
-}
--- Assets/scripts/escenarios/impresora.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class impresora : MonoBehaviour
-{
-   void OnCollisionEnter2D(Collision2D collision)
-    {
-    GetComponent<Renderer>().material.color = Color.blue;
-    }
-}
